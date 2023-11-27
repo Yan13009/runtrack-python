7f1a938be4f7aad64c59f328c3b15ee630cdaad3
yanis  aouimeur <144678545+Yan13009@users.noreply.github.com>
2023-11-27 12:11:56 +0100

    Update job2.py

--- jour5/job2.py
+++ jour5/job2.py
@@ -1,11 +1,18 @@
 def draw_rectangle(width, height):
-    for i in range(height):
-        if i == 0 or i == height - 1:
-            
-            print('-' * width)
-        else:
-          
-            print('|' + ' ' * (width - 2) + '|')
+    if width < 2 or height < 2:
+        print("Les dimensions doivent être supérieures à 1.")
+        return
+
+   
+    print('|' + '-' * (width - 2) + '|')
+
+  
+    for _ in range(height - 2):
+        print('|' + ' ' * (width - 2) + '|')
+
+   
+    if height > 1:
+        print('|' + '-' * (width - 2) + '|')
 
 
 draw_rectangle(10, 3)
